b2dbd484eb7579d16b478e5e9984763c2331d412
Ben Tattersley <ubershinysheep@gmail.com>
2015-01-09 18:54:01 +0000

    test

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<link rel="stylesheet" href="css/assignment.css">
 	</head>
 	<body>
+		<h1>Ben's Amazing Quiz</h1>
 		<script src="js/jquery-1.11.2.min.js"></script>
 		<script src="js/assignment.js"></script>
 	</body>
