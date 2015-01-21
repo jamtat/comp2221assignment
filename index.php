bcfa693319b9afb89c564954ae07f6cce64dfb1a
Ben Tattersley <ubershinysheep@gmail.com>
2015-01-21 17:57:45 +0000

    Fixed fonts giving insecure content warning

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset=UTF-8>
 		<title>COMP2221 Assignment</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-		<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+		<link href='//fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="css/assignment.css">
 	</head>
 	<body class="loading">
