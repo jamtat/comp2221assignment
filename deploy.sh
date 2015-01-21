a9d8885a486bdb1de4ec4c1de2fdf73c7e05fc41
Ben Tattersley <ubershinysheep@gmail.com>
2015-01-21 17:23:39 +0000

    Updated modes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ssh $1@vega.dur.ac.uk "cd ~/public_html/comp2221assignment; rm -rf *"
-rsync -r * $1@vega.dur.ac.uk:~/public_html/comp2221assignment
-ssh $1@vega.dur.ac.uk "~/per"
+#ssh $1@vega.dur.ac.uk "cd ~/public_html/comp2221assignment; rm -rf *"
+#rsync -r * $1@vega.dur.ac.uk:~/public_html/comp2221assignment
+#ssh $1@vega.dur.ac.uk "~/per"
+ssh $1@vega.dur.ac.uk "cd ~/public_html/comp2221assignment; git pull"
